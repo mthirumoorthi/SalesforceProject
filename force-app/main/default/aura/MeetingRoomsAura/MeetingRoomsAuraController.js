0a310d71467db151983c9f880b5d81d2f7348b04
vidhyasri <vidhyasri_Srec@yahoo.co.in>
2022-03-14 22:44:02 -0700

    Commit

--- force-app/main/default/aura/MeetingRoomsAura/MeetingRoomsAuraController.js
+++ force-app/main/default/aura/MeetingRoomsAura/MeetingRoomsAuraController.js
@@ -6,7 +6,8 @@
         {roomno:'C-01',Capacity:'6'},
         {roomno:'B-02',Capacity:'7'},
         {roomno:'B-03',Capacity:'8'},
-        {roomno:'C-01',Capacity:'9'}    
+        {roomno:'C-01',Capacity:'9'},
+        {roomno:'C-01',Capacity:'9'}     
         ]);
         console.log(component.get('v.meetingRoominfo'));
     },
